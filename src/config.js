eba8c8bce6ae6ebf9a02326fb081cbb31d3c22c3
ebbestankiewcz <ebbe2002@me.com>
2025-10-19 22:32:20 +0200

    register form, api utils and basic styling

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const API_BASE = "https://v2.api.noroff.dev";
+export const AUTH_BASE = `${API_BASE}/auth`;
+export const SOCIAL_BASE = `${API_BASE}/social`;
+export const NOROFF_API_KEY = "3c1bb7ae-129f-4bc1-b15f-87aafd3aa909";
+
+export const STORAGE_KEYS = {
+    token: "ns_token",
+    profile: "ns_profile",
+};
