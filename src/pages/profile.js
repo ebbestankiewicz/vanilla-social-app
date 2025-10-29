7df9446649eff35b5fd8aba1a10b57fbcc3d4a62
ebbestankiewcz <ebbe2002@me.com>
2025-10-29 12:11:16 +0100

    add auth guard and dom elemnets

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,20 @@
 import { getFromLocalStorage } from "../utils/storage.js";
 import { STORAGE_KEYS } from "../config.js";
 import { getProfile, getProfilePosts, followProfile, unfollowProfile } from "../api/profiles.js";
+
+// --- Auth guard
+const token = getFromLocalStorage(STORAGE_KEYS.token);
+if (!token) location.href = "./login.html";
+
+
+// --- DOM elements
+const profileTitle = document.getElementById("profileTitle");
+const profileInfo = document.getElementById("profileInfo");
+const avatarImg = document.getElementById("avatar");
+const emailEl = document.getElementById("email");
+const countsEl = document.getElementById("counts");
+const followBtn = document.getElementById("followBtn");
+const unfollowBtn = document.getElementById("unfollowBtn");
+const postsWrap = document.getElementById("profilePosts");
+const whoEl = document.getElementById("who");
+const msgEl = document.getElementById("msg");
