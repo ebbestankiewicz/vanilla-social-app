cf6178ec393898c504453f350cb1d3b19d85926f
ebbestankiewcz <ebbe2002@me.com>
2025-11-10 14:31:39 +0100

    Add: JSDoc to /src/utils/storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,17 @@
+/**
+ * Save a value to localstorage.
+ * @param {string} key 
+ * @param {any} value 
+ */
 export function addToLocalStorage(key, value) {
     localStorage.setItem(key, value);
 }
 
+/**
+ * Get a value from localStorage.
+ * @param {string} key
+ * @returns {any|null}
+ */
 export function getFromLocalStorage(key) {
     return localStorage.getItem(key);
 }
