e32283a2ae618aa840c4de26a4f346cd0ca26c8d
ebbestankiewcz <ebbe2002@me.com>
2025-11-10 14:28:16 +0100

    add: JSDocs to /src/api/post.js

--- src/api/post.js
+++ src/api/post.js
@@ -3,7 +3,12 @@ import { http } from "./http.js";
 import { POSTS_URL } from "../config.js"
 
 /**
- * Get posts.
+ * Fetch a list of posts
+ * @param {Object} options
+ * @param {string} [options.query] - Search query.
+ * @param {number} [options.limit=30] - Number of posts to return.
+ * @param {number} [options.offset=0] - Offset for pagination.
+ * @returns {Promise<any>} List of posts.
  */
 export function listPosts({ query = "", limit = 30, offset = 0 } = {}) {
     const sp = new URLSearchParams({ limit: String(limit), offset: String(offset), _author: "true" });
@@ -12,7 +17,12 @@ export function listPosts({ query = "", limit = 30, offset = 0 } = {}) {
 }
 
 /**
- * Create a post.
+ * Create new post.
+ * @param {Object} data
+ * @param {string} data.title - Post title.
+ * @param {string} data.body - Post body.
+ * @param {string} [data.media] - Optional media URL.
+ * @returns {Promise<any>} Created post.
  */
 export function createPost({ title, body, media }) {
     const payload = { title, body };
@@ -25,6 +35,8 @@ export function createPost({ title, body, media }) {
 
 /**
  * Delete a post by id.
+ * @param {string} id - Post ID.
+ * @returns {Promise<any>} The API delete result.
  */
 export function deletePost(id) {
     return http(`${POSTS_URL}/${id}`, { method: "DELETE" });
@@ -32,6 +44,8 @@ export function deletePost(id) {
 
 /**
  * Get a single post by ID.
+ * @param {string} id - Post ID.
+ * @returns {Promise<any>} The post data.
  */
 export function getPost(id) {
     const sp = new URLSearchParams({ _author: "true" });
@@ -40,6 +54,12 @@ export function getPost(id) {
 
 /**
  * Update a post by ID.
+ * @param {string} id - Post ID.
+ * @param {Object} data
+ * @param {string} [data.title] - New title.
+ * @param {string} [data.body] - New body.
+ * @param {Object} [data.media] - New media object.
+ * @returns {Promise<any>} The updated post data.
  */
 export function updatePost(id, { title, body, media }) {
     const payload = { title, body };
